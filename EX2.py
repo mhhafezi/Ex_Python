3d20bb4aae7e289cdcb20b088f33fe3ab9b0525b
mhhafezi <68470247+mhhafezi@users.noreply.github.com>
2020-07-21 14:22:27 +0430

    Add files via upload

---
+++ EX2.py
@@ -0,0 +1,7 @@
+print('Pop method usage in sets::\n')
+my_set={12,34,56,78,90}
+print('Original my set: ',my_set)
+poped_item=my_set.pop()
+print('Updated my set after pop: ',my_set)
+print('The poped item is ',poped_item)
+
