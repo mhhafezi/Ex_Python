c16fa34e8b2b6e1250015eafcd5848f2d13a6708
mhhafezi <68470247+mhhafezi@users.noreply.github.com>
2020-07-24 20:19:08 +0430

    Add files via upload

--- EX3.py
+++ EX3.py
@@ -14,7 +14,7 @@ months = [31,28,31,30,31,30,31,31,30,30,30,31]
 if leap == True :
     months[1] = 29
 m=0
-for i in range(1,Month):
+for i in range(0,Month):
     m += months[i] 
 dayofyear = m + Day
 print("Today is ",dayofyear ," day of the year")
---
+++ EX4.py
@@ -0,0 +1,92 @@
+#012
+"""
+first=input('Enter your FIRST number-int or float:')
+second=input('Enter your SECOND number-int or float:')
+
+
+if (float(first)>float(second)) :
+    print('Second number',second)
+    print('First number',first)
+else:
+    print('First number',first)
+    print('Second number',second)
+"""
+
+#013
+"""
+Number=float(input('Enter a number under 20-int or float:'))
+if (Number == 20) or (Number >= 20):
+    print('"Too high"')
+else:
+    print('"Thank you"')
+"""
+
+#014
+"""
+Number=float(input('Enter a number between 10 and 20 (inclusive)-int or float:'))
+if 10<= Number <=20 :
+    print('"Thank you"')
+else:
+    print('"Incorrect answer"')
+"""
+
+#015
+"""
+color=input('Enter Your Favorite Color:')
+if color=='red' or color=='RED' or color=='Red':
+    print('"I like red too"')
+else:
+    print("I don't like",color,", I prefer red")
+"""
+
+#016
+"""
+answer= input('Is it raining?:')
+answer=answer.lower()
+if answer=='yes':
+    answerw= input('Is it windy?:')
+    answerw=answerw.lower()
+    if answerw == 'yes':
+        print('"It is too windy for an umbrella"')
+    else:
+        print('"Take an umbrella"')
+else:
+    print('"Enjoy your day"')
+"""
+
+#017
+"""
+age=int(input('Enter Your Age:'))
+if age>= 18 :
+    print('You can vote')
+elif age == 17:
+    print('You can learn to drive')
+elif age == 16:
+    print('You can buy a lottery ticket')
+elif  age < 16:
+    print('You can go Trick-or-Treating')
+"""
+
+#018
+"""
+Number=float(input('Enter a number -int or float:'))
+if Number < 10:
+    print("Too low")
+elif 10<= Number <=20 :
+    print("Correct")
+else:
+    print("Too high")
+"""
+
+#019
+"""
+Number=input('Enter 1, 2 or 3:')
+if int(Number) == 1:
+    print('"Thank you"')
+elif int(Number) == 2:
+    print('"Well done"')
+elif int(Number) == 3:
+    print('"Correct"')
+else:
+    print("Error message")
+"""
