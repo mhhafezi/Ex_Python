3fdc644fa6ce187519448972d323db8f0dbfb0a0
mhhafezi <68470247+mhhafezi@users.noreply.github.com>
2020-07-26 21:39:49 +0430

    Add files via upload

--- EX7.py
+++ EX7.py
@@ -6,4 +6,18 @@ def Fibonacci(n):
     elif n==2: 
         return 1
     else: 
-        return Fibonacci(n-1)+Fibonacci(n-2) 
+        return Fibonacci(n-1)+Fibonacci(n-2)
+
+"""
+def Fibonacci_list(n):
+    L=[0,1]
+    for i in range(n-2):
+        L.append(L[-1]+L[-2])
+    return L
+"""
+
+def Fibonacci_list(n):
+    L=[0,1]
+    for i in range(3,n+1):
+        L.append(Fibonacci(i))
+    return L
